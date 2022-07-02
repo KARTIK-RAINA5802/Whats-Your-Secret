ad6f6461e74a5bed3241a1b8de28043e96be17f4
Kartik Raina <96180683+KARTIK-RAINA5802@users.noreply.github.com>
2022-07-02 13:48:51 +0530

    installed and connected to mongodb using mongoose

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose")
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 app.get("/", function(req, res){
     res.render("home");
